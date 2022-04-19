19a31b40aa097a102657e17adeb03990b3568b3f
Penguin.Kim <cutlake2000@naver.com>
2022-04-20 02:32:43 +0900

    스택

---
+++ Stack_04.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_STACK_SIZE 100
+typedef int element;
+typedef struct{
+    element data[MAX_STACK_SIZE];
+    int top;
+} StackType;
+
+// 스택 초기화 함수
+void init_Stack(StackType* s){
+    s->top = -1;
+}
+
+// 공백 상태 검출 함수
+int is_Empty(StackType* s){
+    return (s->top == -1);
+}
+
+// 포화 상태 검출 함수
+int is_Full(StackType* s){
+    return (s->top == (MAX_STACK_SIZE - 1));
+}
+
+// 삽입 함수
+void push(StackType* s, element item){
+    if (is_Full(s)){
+        fprintf(stderr, "스택 포화 에러\n");
+        exit(1);
+    }
+    else s->data[++(s->top)] = item;
+}
+
+// 삭제 함수
+element pop(StackType* s){
+    if (is_Empty(s)){
+        fprintf(stderr, "스택 공백 에러\n");
+        exit(1);
+    }
+    else return s->data[(s->top)--];
+}
+
+// 피크 함수
+element peek(StackType* s){
+    if (is_Empty(s)){
+        fprintf(stderr, "스택 공백 에러\n");
+        exit(1);
+    }
+    else return s->data[(s->top)];
+}
+
+int main(void){
+    StackType* s;
+    s = (StackType*) malloc(sizeof(StackType));
+
+    init_Stack(s);
+    push(s, 5);
+    push(s, 6);
+    push(s, 7);
+    printf("%d\n", pop(s));
+    printf("%d\n", pop(s));
+    printf("%d\n", pop(s));
+    free(s);
+}
